cf187095c400e5252e8c2ade161887a8adf92a6e
Miguel <miguelangelmorenofp@gmail.com>
2026-03-10 18:30:09 +0100

    añadidos casos de uso, controlador, filtro de errores centralizado y módulo de users

--- messaging-api-nestjs/src/app.module.ts
+++ messaging-api-nestjs/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './modules/users/users.module';
+import { UserOrmEntity } from './modules/users/infrastructure/typeorm/user.typeorm-entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [UserOrmEntity],
+      synchronize: true, // solo en desarrollo, nunca en producción
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
